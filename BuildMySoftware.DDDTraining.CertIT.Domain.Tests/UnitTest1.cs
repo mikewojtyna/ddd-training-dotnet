798210f5044d4ba1c2179e0ff3cc6cbddef454f4
Mike Wojtyna <mike@buildmysoftware.pro>
2019-10-15 00:14:14 +0200

    Add CertIT project description

---
+++ BuildMySoftware.DDDTraining.CertIT.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BuildMySoftware.DDDTraining.CertIT.Domain.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
