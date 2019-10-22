046240b429b5a59140c2d916cde424bd815353e2
Mike Wojtyna <mike@buildmysoftware.pro>
2019-10-22 13:47:20 +0200

    Add basic bike rental scenario examples

--- BuildMySoftware.DDDTraining.Bike.Domain.Tests/BikeTests.cs
+++ BuildMySoftware.DDDTraining.Bike.Domain.Tests/BikeTests.cs
@@ -11,16 +11,54 @@ namespace BuildMySoftware.DDDTraining.Bike.Domain.Tests
         }
 
         [Test]
-        public void CanRentABike()
+        public void Given_BikeRackWithSingleBike_ThenCanRentABike()
         {
             // given
-            Bike bike = new Bike();
-            
+            BikeRack bikeRack = BikeRackWithSingleBike();
+
             // when
-            BikeRent bikeRent = bike.Rent();
+            BikeRent bikeRent = bikeRack.Rent();
 
             // then
             Check.That(bikeRent).IsNotNull();
+        }
+
+        [Test]
+        public void Given_EmptyBikeRack_When_RentABike_ThenRentFails()
+        {
+            // given
+            BikeRack bikeRack = EmptyBikeRack();
+
+            // when
+            Check.ThatCode(() => bikeRack.Rent())
+
+                // then
+                .Throws<FailedToRentABike>();
+        }
+
+        [Test]
+        public void Given_BikeRackWithSingleBike_When_RentABikeTwice_ThenOnlySecondRentFails()
+        {
+            // given
+            BikeRack bikeRack = BikeRackWithSingleBike();
+
+            // when
+            var bikeRent = bikeRack.Rent();
+            Check.ThatCode(() => bikeRack.Rent())
+
+                // then
+                .Throws<FailedToRentABike>();
+            Check.That(bikeRent).IsNotNull();
+        }
+
+        private BikeRack EmptyBikeRack()
+        {
+            return BikeRack.Empty();
+        }
+
+        private BikeRack BikeRackWithSingleBike()
+        {
+            return BikeRack.WithSingleBike();
         }
     }
 }
---
+++ BuildMySoftware.DDDTraining.Bike.Domain.Tests/FailedToRentABike.cs
@@ -0,0 +1,6 @@
+namespace BuildMySoftware.DDDTraining.Bike.Domain.Tests
+{
+    public class FailedToRentABike : System.Exception
+    {
+    }
+}
